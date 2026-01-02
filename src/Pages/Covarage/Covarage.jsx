cf275f94eb8ce24e5c6a87c17542314580edb0be
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2026-01-03 01:43:54 +0600

    map marker

--- src/Pages/Covarage/Covarage.jsx
+++ src/Pages/Covarage/Covarage.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'
 
 const Covarage = () => {
+    const position = [24.3745,88.6042]
+
     return (
-        <div>
-            <h1>We Are Available In 64 Districs</h1>
+        <div >
+            <h1 className='text-4xl font-bold text-center my-6 '>We Are Available In 64 Districs</h1>
+            <div></div>
+
+            <div className='w-full h-150'>
+
+                <MapContainer center={position} zoom={7} scrollWheelZoom={false} className='h-150'>
+                  <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+          <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+                </MapContainer>
+            </div>
         </div>
     );
 };
